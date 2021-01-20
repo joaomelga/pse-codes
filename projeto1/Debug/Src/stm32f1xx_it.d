5efec97a3520fddbdb29a7f17d70eecb516d7d8f
joaomelga <joaolucasfm@gmail.com>
2021-01-20 15:17:30 -0400

    Initial commit

---
+++ projeto1/Debug/Src/stm32f1xx_it.d
@@ -0,0 +1,73 @@
+Src/stm32f1xx_it.o: ../Src/stm32f1xx_it.c \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/main.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/stm32f1xx_hal_conf.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/core_cm3.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_version.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/stm32f1xx_it.h
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/main.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/stm32f1xx_hal_conf.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/core_cm3.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_version.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+E:/Users/joaom/Documents/Cursos/Programação\ de\ sistemas\ embarcados/Projetos/projeto1/Inc/stm32f1xx_it.h:
